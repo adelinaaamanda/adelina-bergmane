661d1a4468f85f4269e6317d1f334db32afbf345
adelinaaamanda <abergmanea@gmail.com>
2025-02-14 10:25:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+/*
+alert("hello world");
+
+console.log("dab");
+*/
+
+/*
+let x = 5;
+let y = 5;
+
+if(x==y){
+  console.log("x ir vienāds ar y");
+}
+*/
+
+
+let number = 10;
+if(number > 0)
+{
+    console.log("pozitīvs");
+}
+if (number < 0)
+{
+    console.log("negatīvs");
+}
+if (number == 0)
+{
+    console.log("ir 0");
+}
