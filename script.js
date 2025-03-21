ca3abdb8babe446de5050f46f48f52b111c96086
adelinaaamanda <abergmanea@gmail.com>
2025-03-21 09:35:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-/*
-alert("hello world");
+let a = 10;
+let b = 5;
 
-console.log("dab");
-*/
 
-/*
-let x = 5;
-let y = 5;
+console.log("Summas rezultāts: " + (a + b));
+console.log("Starpības rezultāts: " + (a - b));
+console.log("Reizinājuma rezultāts: " + (a * b));
+console.log("Dalījuma rezultāts: " + (a / b));
 
-if(x==y){
-  console.log("x ir vienāds ar y");
+
+console.log("a pakāpē b: " + Math.pow(a, b));
+console.log("a dalījuma atlikums dalot ar b: " + (a % b));
+
+
+if (a > b) {
+    console.log("a ir lielāks par b");
+} else if (a == b) {
+    console.log("a ir vienāds ar b");
+} else if (b < a) {
+    console.log("b ir mazāks par a");
 }
-*/
 
 
-let number = 10;
-if(number > 0)
-{
-    console.log("pozitīvs");
+switch (true) {
+    case (a > b):
+        console.log("a ir lielāks par b");
+        break;
+    case (a == b):
+        console.log("a ir vienāds ar b");
+        break;
+    case (b < a):
+        console.log("b ir mazāks par a");
+        break;
 }
-if (number < 0)
-{
-    console.log("negatīvs");
+
+
+let masivs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+for (let i = 0; i < masivs.length; i++) {
+    console.log("Masīva elements: " + masivs[i]);
 }
-if (number == 0)
-{
-    console.log("ir 0");
+
+
+for (let i = masivs.length - 1; i >= 0; i--) {
+    console.log("Masīva elements apgrieztā secībā: " + masivs[i]);
 }
